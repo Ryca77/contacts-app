44e2010ddab826073cd2277d460f2e47add26cd0
Ryca77 <ryan.carter77@gmail.com>
2016-06-07 19:17:09 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 
+//create contact object//
 $('.add').on('click', function() {
-	var i = 0;
 	var contactObj = {
-		'First Name':
+		'FirstName':
 		$('[class=first-name]').val(),
-		'Last Name':
+		'LastName':
 		$('[class=last-name]').val(),
 		'Phone':
 		$('[class=phone]').val(),
-		'Address 1':
+		'Address1':
 		$('[class=add1]').val(),
-		'Address 2':
+		'Address2':
 		$('[class=add2]').val(),
 		'County':
 		$('[class=county]').val(),
 	}
 	console.log(contactObj);
 	console.log('record');
-	generateRecord();
 
+	//empty input fields//
 	$('.first-name').val('');
 	$('.last-name').val('');
 	$('.phone').val('');
@@ -27,37 +27,53 @@ $('.add').on('click', function() {
 	$('.add2').val('');
 	$('.county').val('');
 
-function generateRecord() {
-var fName = $('.first-name').val();
+//create contact record and contact list from object//
+var fName = contactObj.FirstName;
+var lName = contactObj.LastName;
+var pNumber = contactObj.Phone;
+var aOne = contactObj.Address1;
+var aTwo = contactObj.Address2;
+var county = contactObj.County;
+var contactRecord = fName + " " + lName + "<br>" + "First Name: " + fName
+	 + "<br>" + "Last Name: " + lName + "<br>" + "Phone: " + " " + pNumber + "<br>"
+	  + "Address: " + " " + aOne + ", " + aTwo + ", " + county;
+var cList = fName + " " + lName;
+
+$('.contact-record').html(contactRecord);
+$('.clist').append(cList + "<br>");
+
+//store contact record//
+localStorage.setItem('.contact-record', contactRecord);
+var retrieveRecord = localStorage.getItem('.contact-record');
+$('.clist').on('click', function() {
+console.log(retrieveRecord);
+})
+
+//fix... clicking on contacts list console logs both contacts entered//
+//write code which displays contact clicked into contact record//
+
+});
+
+});
+
+
+
+/*var fName = $('.first-name').val();
 var lName = $('.last-name').val();
-	$('.contact-record').html("Name:" + " " + fName + " " + lName);
-	console.log("Name:" + " " + fName + " " + lName);
-}
+var pNumber = $('.phone').val();
+var aOne = $('.add1').val();
+var aTwo = $('.add2').val();
+var county = $('.county').val();*/
 
-});
-
-
-
-});
-
-/*var fName = contactObj[i].firstName;
-var lName = contactObj[i].lastName;
-var pNumber = contactObj[i].phone;
-var aOne = contactObj[i].address1;
-var aTwo = contactObj[i].address2;
-var county = contactObj[i].county;*/
-
-
-/*function generateRecord() {
-	var dataEntered = document.getElementById("first-name").value;
-	document.getElementById("fname").innerHTML = dataEntered;
-	var dataEntered = document.getElementById("last-name").value;
-	document.getElementById("lname").innerHTML = dataEntered;
-	var dataEntered = document.getElementById("phone").value;
-	document.getElementById("pnumber").innerHTML = dataEntered;
-	var dataEntered = document.getElementById("add1").value;
-	document.getElementById("aone").innerHTML = dataEntered;
-	var dataEntered = document.getElementById("add2").value;
-	document.getElementById("atwo").innerHTML = dataEntered;
-	var dataEntered = document.getElementById("county").value;
-	document.getElementById("count").innerHTML = dataEntered;*/
+/*var dataEntered = document.getElementById("first-name").value;
+document.getElementById("fname").innerHTML = dataEntered;
+var dataEntered = document.getElementById("last-name").value;
+document.getElementById("lname").innerHTML = dataEntered;
+var dataEntered = document.getElementById("phone").value;
+document.getElementById("pnumber").innerHTML = dataEntered;
+var dataEntered = document.getElementById("add1").value;
+document.getElementById("aone").innerHTML = dataEntered;
+var dataEntered = document.getElementById("add2").value;
+document.getElementById("atwo").innerHTML = dataEntered;
+var dataEntered = document.getElementById("county").value;
+document.getElementById("count").innerHTML = dataEntered;*/
